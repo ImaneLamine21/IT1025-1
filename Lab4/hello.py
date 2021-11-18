f76b147dfb8998f4f2146ee502fdc78f67e853e1
ImaneLamine21 <90275537+ImaneLamine21@users.noreply.github.com>
2021-11-17 19:45:16 -0500

    Add files via upload

---
+++ Lab4/hello.py
@@ -0,0 +1,24 @@
+Python 3.9.7 (v3.9.7:1016ef3790, Aug 30 2021, 16:39:15) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # nov17,2021. Imane Lamine
+
+
+print('Hello world!')
+
+ print ('What is your name?') # ask for their name
+ 
+myName=input ()
+
+print('It is good to meet you,+ myName)
+
+print ('The length of your name is:')
+
+print (len (myName))
+
+print('What is your age?') # ask for their age
+
+myAge= input ()
+
+ print ('You will be+ str(int (myAge) + 1) + in a year.')
+                                    
